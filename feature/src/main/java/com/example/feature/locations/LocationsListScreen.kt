9e4944980782dbdd8887198db4ef449608694dbc
OscarD8 <oscardavison8@outlook.com>
2025-10-06 18:48:43 +0100

    chore: renaming feature module back to UI (makes more sense). Started process of returning different data depending on ViewModel calls for specific screens.

--- feature/src/main/java/com/example/feature/locations/LocationsListScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.feature.locations
-
-data class LocationsListScreen<T> (
-    val isLoading: Boolean = false,
-    val items: List<T> = emptyList(),
-    val error: String? = null
-)
-
